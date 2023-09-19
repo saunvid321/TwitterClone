d9c10533197cebdbfc7cd7451345058acda9e362
adityakhavanekar <saunvidganbavale100@gmail.com>
2023-09-19 11:25:16 +0530

    Added Files

---
+++ twitter-server/src/app/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+export async function initServer() {
+    const app=express();
+
+    app.use(bodyParser.json());
+    const graphqlserver = new ApolloServer({
+        typeDefs: `
+            type Query{
+                sayHello:String
+            }
+        `,
+        resolvers: {
+            Query:{
+                sayHello: ()=> `hello from Graphql Server`,
+            },
+        },
+      });
+    
+      await graphqlserver.start();
+
+      app.use('/graphql', expressMiddleware(graphqlserver));
+
+      return app;
+    
+}
