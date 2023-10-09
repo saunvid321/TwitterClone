9ade4d5e2f742005647c71f14855fe2ed5331be9
saunvid <saunvidganbavale100@gmail.com>
2023-10-08 19:29:44 -0700

    Added user profile
    change files Added user profile and tweet feed

---
+++ twitter-clone-8c98c2cba6007bb5209e6ab5fcff5427f10f2e77/pages/index.tsx
@@ -0,0 +1,149 @@
+import React, { useCallback } from "react";
+import Image from "next/image";
+import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
+import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { SlOptions } from "react-icons/sl";
+
+import FeedCard from "@/components/FeedCard";
+import { toast } from "react-hot-toast";
+import { graphqlClient } from "@/clients/api";
+import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
+import { useCurrentUser } from "@/hooks/user";
+import { useQueryClient } from "@tanstack/react-query";
+
+interface TwitterSidebarButton {
+  title: string;
+  icon: React.ReactNode;
+}
+
+const sidebarMenuItems: TwitterSidebarButton[] = [
+  {
+    title: "Home",
+    icon: <BiHomeCircle />,
+  },
+  {
+    title: "Explore",
+    icon: <BiHash />,
+  },
+  {
+    title: "Notifications",
+    icon: <BsBell />,
+  },
+  {
+    title: "Messages",
+    icon: <BsEnvelope />,
+  },
+  {
+    title: "Bookmarks",
+    icon: <BsBookmark />,
+  },
+  {
+    title: "Twitter Blue",
+    icon: <BiMoney />,
+  },
+  {
+    title: "Profile",
+    icon: <BiUser />,
+  },
+  {
+    title: "More Options",
+    icon: <SlOptions />,
+  },
+];
+
+export default function Home() {
+  const { user } = useCurrentUser();
+  const queryClient = useQueryClient();
+
+  console.log(user);
+
+  const handleLoginWithGoogle = useCallback(
+    async (cred: CredentialResponse) => {
+      const googleToken = cred.credential;
+      if (!googleToken) return toast.error(`Google token not found`);
+
+      const { verifyGoogleToken } = await graphqlClient.request(
+        verifyUserGoogleTokenQuery,
+        { token: googleToken }
+      );
+
+      toast.success("Verified Success");
+      console.log(verifyGoogleToken);
+
+      if (verifyGoogleToken)
+        window.localStorage.setItem("__twitter_token", verifyGoogleToken);
+
+      await queryClient.invalidateQueries(["curent-user"]);
+    },
+    [queryClient]
+  );
+
+  return (
+    <div>
+      <div className="grid grid-cols-12 h-screen w-screen px-56">
+        <div className="col-span-3 pt-1 ml-28 relative">
+          <div className="text-2xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transition-all">
+            <BsTwitter />
+          </div>
+          <div className="mt-1 text-xl pr-4">
+            <ul>
+              {sidebarMenuItems.map((item) => (
+                <li
+                  className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-3 py-3 w-fit cursor-pointer mt-2"
+                  key={item.title}
+                >
+                  <span className="text-3xl">{item.icon}</span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-5 px-3">
+              <button className="bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full">
+                Tweet
+              </button>
+            </div>
+          </div>
+          {user && (
+            <div className="absolute bottom-5 flex gap-2 items-center bg-slate-800 px-3 py-2 rounded-full">
+              {user && user.profileImageURL && (
+                <Image
+                  className="rounded-full"
+                  src={user?.profileImageURL}
+                  alt="user-image"
+                  height={50}
+                  width={50}
+                />
+              )}
+              <div>
+                <h3 className="text-xl">
+                  {user.firstName} {user.lastName}
+                </h3>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-scroll border-gray-600">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
+        <div className="col-span-3 p-5">
+          {!user && (
+            <div className="p-5 bg-slate-700 rounded-lg">
+              <h1 className="my-2 text-2xl">New to Twitter?</h1>
+              <GoogleLogin onSuccess={handleLoginWithGoogle} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
