e2a53f3b0d5d5e430b2a457e5646312bbee6af66
saunvid <saunvidganbavale100@gmail.com>
2023-10-23 14:01:07 -0700

    added follow unfollow

--- twitter-clone-client/pages/[id].tsx
+++ twitter-clone-client/pages/[id].tsx
@@ -8,6 +8,10 @@ import FeedCard from "@/components/FeedCard";
 import { Tweet, User } from "@/gql/graphql";
 import { graphqlClient } from "@/clients/api";
 import { getUserByIdQuery } from "@/graphql/query/user";
+import { useCallback, useMemo } from "react";
+import { userInfo } from "os";
+import { useQueries, useQueryClient } from "@tanstack/react-query";
+import { followUserMutation, un } from "@/graphql/mutation/user";
 
 interface ServerProps {
   userInfo?: User;
@@ -15,7 +19,23 @@ interface ServerProps {
 
 const UserProfilePage: NextPage<ServerProps> = (props) => {
   const router = useRouter();
+  const {user:currentUser}=useCurrentUser();
+  const queryClient=useQueryClient();
 
+  const amIfollowing=useMemo(()=>{
+    if(!props.userInfo) return false;
+    return ( props.userInfo.followers?.findIndex((e)=>e?.id===currentUser?.id)??-1  )>=0
+
+  },[props.userInfo, currentUser?.id]);
+
+  const handleFollowUser=useCallback( async()=>{
+    if(!props.userInfo?.id) return ;
+    await graphqlClient.request(followUserMutation,{to: props.userInfo?.id});
+    await queryClient.invalidateQueries(['current-user']);
+
+
+  },[props.userInfo?.id, queryClient]);
+  
   return (
     <div>
       <Twitterlayout>
@@ -40,8 +60,33 @@ const UserProfilePage: NextPage<ServerProps> = (props) => {
               />
             )}
             <h1 className="text-2xl font-bold mt-5">{props.userInfo?.firstName} {props.userInfo?.lastName}</h1>
+            <div className="flex justify-between items-center ">
+            <div className="flex gap-4 mt-2 text-sm text-gray-400">
+              
+              <span> {props.userInfo?.followers?.length} Followers</span>
+              <span> {props.userInfo?.following?.length} Following</span>
+            </div>
+            {
+              currentUser?.id!==props.userInfo?.id &&
+              (
+                <>
+                  {amIfollowing? (
+                    <button className=" bg-[#1d9bf0] text-white px-3 py-1 rounded-full text-sm ">
+                      Unfollow</button>
+                  ):(
+                    <button onClick={handleFollowUser} className=" bg-[#1d9bf0] text-white px-3 py-1 rounded-full text-sm ">
+                      Follow</button>
+
+                  )}
+                </>
+              )
+
+              
+              
+            }
+            </div>
           </div>
-          <div>
+          <div> 
             {props.userInfo?.tweets?.map((tweet) => (
               <FeedCard data={tweet as Tweet} key={tweet?.id} />
             ))}
