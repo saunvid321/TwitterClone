a0d5f3a2817be2be311ba37f2378166a47037359
saunvid <saunvidganbavale100@gmail.com>
2023-11-10 10:38:41 -0800

    added health group

--- twitter-clone-server/src/clients/redis/index.ts
+++ twitter-clone-server/src/clients/redis/index.ts
@@ -1,3 +1,3 @@
 import Redis from "ioredis"
 
-export const redisClient = new Redis("redis://default:e3f9a2f470b94a4f9ef744eb167f1584@usw1-heroic-badger-33314.upstash.io:33314");
+export const redisClient = new Redis(process.env.REDIS_URL as string);
